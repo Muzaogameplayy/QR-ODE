ae7513d0641851facd0087968b9fa0a89ceef189
Muzaogameplayy <135380701+Muzaogameplayy@users.noreply.github.com>
2023-08-25 17:21:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ generateBtn = wrapper.querySelector(".form button").
 qrImg = wrapper.querySelector(".qr- code img");
 let preValue;
 
-generateBtn.addEventlitener("click", ) =>
+generateBtn.addEventlitener ("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue ||  preValue === qrValue) return;
+    preValue  = qrValue;
+    generateBtn.innerText = "Generation QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventlitener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventLitener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+})
